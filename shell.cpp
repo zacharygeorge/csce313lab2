f90981ea533221cb413bc61116ec7791a1df54e8
zacharygeorge <zachg4393@gmail.com>
2025-09-14 01:55:13 +0000

    tested and passed

--- shell.cpp
+++ shell.cpp
@@ -2,7 +2,10 @@
 LE2: Introduction to Unnamed Pipes
 ****************/
 #include <unistd.h> // pipe, fork, dup2, execvp, close
+#include <iostream>
+#include <sys/wait.h> 
 using namespace std;
+
 
 int main () {
     // lists all the files in the root directory in the long format
@@ -12,16 +15,62 @@ int main () {
 
     // TODO: add functionality
     // Create pipe
+    int fds[2];
+    if (pipe(fds) == -1) {
+        cerr << "pipe failed\n";
+        return 1;
+    }
 
     // Create child to run first command
-    // In child, redirect output to write end of pipe
-    // Close the read end of the pipe on the child side.
-    // In child, execute the command
-
+    pid_t pid = fork();
+    if (pid == -1) {
+        cerr << "fork failed\n";
+        return 1;
+    }
+    if (pid == 0) {
+        // In child, redirect output to write end of pipe
+        //replace child with cmd1
+        dup2(fds[1],1);
+        close(fds[1]); // close write end
+        // Close the read end of the pipe on the child side.
+        close(fds[0]);
+        // In child, execute the command
+        execvp(cmd1[0], cmd1);
+        // If execvp returns, something went wrong
+        cerr << "exec failed\n";
+        return 1;
+    } else {
+        // close(fds[0]);
+        // close(fds[1]);
+        // wait(nullptr); // wait for child
+    }
+    
     // Create another child to run second command
-    // In child, redirect input to the read end of the pipe
-    // Close the write end of the pipe on the child side.
-    // Execute the second command.
+    pid = fork();
+
+    if (pid == -1) {
+        cerr << "fork failed\n";
+        return 1;
+    }
+    if (pid == 0) {
+        // In child, redirect input to the read end of the pipe
+        dup2(fds[0], 0);
+        // Close the write end of the pipe on the child side.
+        close(fds[0]);//close read end
+        close(fds[1]); //close write end
+        // Execute the second command.
+        execvp(cmd2[0], cmd2);
+        // If execvp returns, something went wrong
+        cerr << "exec failed\n";
+        return 1;
+    } else {
+        close(fds[0]);
+        close(fds[1]);
+        wait(nullptr); // wait for child
+    }
+    
+    
+    
 
     // Reset the input and output file descriptors of the parent.
 }
